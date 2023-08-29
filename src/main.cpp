34bc18860f5d2451f30a61605db3d47ef3cf95f5
Cesar Nicolas Aguilar <143230389+CompaNico@users.noreply.github.com>
2023-08-29 01:51:48 +0000

    libreria iostream

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,6 @@
-
+#include <iostream>
 
 int main(int argc, char const *argv[])
 {
-    for (size_t 3 = 0; 3 < 10; 3++)
-    {
-        /* code */
-    }
-    
-    return 0;
-}
-
-primer codigo
+    std::cout<<"Mensaje"<<std::endl;   
+}  

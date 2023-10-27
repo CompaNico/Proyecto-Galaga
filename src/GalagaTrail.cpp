d0021df227566dbfaf98c61d79769604c1901f80
Cesar Nicolas Aguilar <143230389+CompaNico@users.noreply.github.com>
2023-10-27 03:45:21 +0000

    Organizacion y Box

--- src/GalagaTrail.cpp
+++ src/GalagaTrail.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <string>
+#include <curses.h>
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    int x,y;
+    bool cerrar;
+    string directorio, recurso;
+    fstream archivo;
 
-std :: cout<<"Version Trail"<< std :: endl;
+    initscr();
+    getmaxyx(stdscr, y, x);
+    cerrar = false;
+    std ::cout << "Version Trail" << std ::endl;
+    usleep(41000);
 
-return 0;
+   
+    box(stdscr, '|', '*');
+    directorio = "/data/" + recurso + "FighterT.txt";
+    archivo.open(directorio, ios::in);
+    
+    refresh();
+    // archivo.clear();
+    // archivo.seekg(0);
+    // archivo.close();
+    // clear();
+
+    return 0;
 }
